4d12195a3bca96b7fc2c1bd1b2daa282a1300c60
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-08-29 17:46:48 +0600

    Task #4. Primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        double formula;
+        int a = 1;
+        int b = 2;
+
+        formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(formula);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int char1 = 99;
+        int char2 = 104;
+        int char3 = 97;
+        int char4 = 114;
+
+        char c1 = (char) char1;
+        char c2 = (char) char2;
+        char c3 = (char) char3;
+        char c4 = (char) char4;
+        System.out.print(c1 + "," + c2 + "," +c3 + "," + c4);
     }
 }
